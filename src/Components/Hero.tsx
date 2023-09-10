a0a980165ff1bc580ae94f11addd4488f3ffee30
Ashakuur1 <engshakrayare114@gmail.com>
2023-09-10 21:29:51 +0300

    i added some styling

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <div className="flex justify-center items-center mt-[260px] text-center">
-      <div>
-        <h1 className="text-4xl">Donate your Blood</h1>
+    <div className="flex justify-center items-center  text-center">
+      <div className="mt-[150px]">
+        <h1 className="text-4xl font-bold">Donate your Blood</h1>
         <p className="w-[600px] text-xl mt-3">
           Blood donation is the act of voluntarily giving blood, either whole
           blood or specific blood components, to be used for medical purposes.
@@ -15,7 +15,32 @@ const Hero = () => {
             Donor
           </Button>
         </Link>
+        <h1 className="text-2xl text-white mt-5 font-semibold	">Requirments </h1>
+        <div className="grid grid-cols-3 gap-8 ">
+        <div className="w-[200px] shadow-xl flex  p-4 my-4 rounded-lg hover:scale-105 duration-300 bg-white">
+          
+          <ul  className="text-black  text-left mx-5 mt-5">
+            <li><span className="font-bold">Age:</span> Typically 17 years or older.</li>
+
+          </ul>
+        </div>
+        <div className="w-[200px] shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 bg-black">
+          
+          <ul  className="text-white  text-left mx-5 mt-5">
+      <li><span className="font-bold">Weight:</span> Usually a minimum of 50 kilograms (110 pounds).</li>
+          </ul>
+        </div>
+        <div className="w-[200px] shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 bg-white">
+          
+          <ul  className="text-black  text-left mx-5 mt-5">
+
+            <li><span className="font-bold	">Health:</span> Good overall health and passing a medical screening. </li>
+          </ul>
+        </div>
       </div>
+      </div>
+      
+
     </div>
   );
 };
